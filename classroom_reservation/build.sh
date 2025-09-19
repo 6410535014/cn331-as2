575144ba519333807aba28ceac97cb3440bc4008
Ukrit Dokkularb <6410535014@cn331>
2025-09-20 00:24:04 +0700

    update build.sh

--- classroom_reservation/build.sh
+++ classroom_reservation/build.sh
@@ -5,5 +5,5 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py createsuperuser --username admin --email "your@email.com" --noinput || true
+python manage.py migrate 
+python manage.py loaddata initial_data.json || true
